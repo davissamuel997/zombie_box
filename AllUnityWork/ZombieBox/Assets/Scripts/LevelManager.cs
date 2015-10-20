812e62c1c78e161897230b568ef2542ac311418f
Eric_Momper <emomper23@gmail.com>
2015-10-19 22:49:49 -0400

    added fbx and other components

---
+++ AllUnityWork/ZombieBox/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class LevelManager : MonoBehaviour {
+
+    EnemyManager enemy_mng;
+    TurretManager turret_mng;
+    BaseManager base_mng; 
+        
+    // Use this for initialization
+    void Start ()
+    {
+        base_mng = GameObject.FindObjectOfType<BaseManager>();
+        enemy_mng = GameObject.FindObjectOfType<EnemyManager>();
+        turret_mng = GameObject.FindObjectOfType<TurretManager>();
+        enemy_mng.spawnEnemies(20);
+        turret_mng.setEnemyList(enemy_mng.getEnemyList());
+        
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+}
