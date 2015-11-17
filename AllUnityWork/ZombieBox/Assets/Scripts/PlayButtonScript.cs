76c9f6d6101f6fc5f2284dbe075ba5a5b380fbad
Zenairo <peterlomason@gmail.com>
2015-11-16 20:42:42 -0500

    main menu

--- AllUnityWork/ZombieBox/Assets/Scripts/PlayButtonScript.cs
+++ AllUnityWork/ZombieBox/Assets/Scripts/PlayButtonScript.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Text.RegularExpressions;
 
 public class PlayButtonScript : MonoBehaviour {
+
 	public void PlayPressed ()
 	{
 
 		PlayerPrefs.SetString("charID", Regex.Replace(CharSelect.selected.name, "[^0-9]", ""));
-		Application.LoadLevel ("base");              
+		//mainCamera.transform.Rotate(Vector3.forward, 10.0f * Time.deltaTime);
+
+		//Application.LoadLevel ("base");              
 	}
 }
